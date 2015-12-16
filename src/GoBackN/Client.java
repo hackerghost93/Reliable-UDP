ed177b406570e9993fb366a4be7a92ea36b6fc0f
HassanRezk <hassan.rms93@gmail.com>
2015-12-16 13:33:12 +0200

    Modify GoBackN.

--- src/GoBackN/Client.java
+++ src/GoBackN/Client.java
@@ -6,25 +6,27 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.util.Scanner;
 import java.util.zip.CRC32;
 
 public class Client {
-
+	
 	public static int clientPort;
+	public static int serverPort;
 	public static DatagramSocket socket;
+	
 	public static double probability;
+	
+	public static InetAddress ip;
+	
 	static byte awaiting;
 	static byte window;
 	static DatagramPacket[] packetsReceived;
 	static ByteBuffer bufferR = ByteBuffer.allocate(570);
-
-	public static InetAddress ip;
-	public static int serverPort;
-
 	public static synchronized byte getSeqnum(DatagramPacket packet) {
 		return packet.getData()[0];
 	}
-
+	
 	public static void WriteFile(byte[] data) throws IOException {
 		FileOutputStream output = new FileOutputStream("out.txt", true);
 		for (int i = 1; i < data.length && data[i] != 0; ++i)
@@ -32,6 +34,7 @@ public class Client {
 		output.close();
 		data = null;
 	}
+	
 
 	public static int MOD;
 
@@ -43,10 +46,10 @@ public class Client {
 		DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length, ip,
 				serverPort);
 		double num = Math.random();
-		if (num > probability) {
+		 if(num > probability) {
 			socket.send(packet);
 			System.out.println("Sending ack for " + seqnum);
-		} else System.out.println("drop ack for " + seqnum);
+		 } else System.out.println("Drop ack for " + seqnum);
 	}
 
 	private static void IncWait() {
@@ -67,44 +70,53 @@ public class Client {
 	public static void main(String[] args) throws IOException {
 		new FileOutputStream("out.txt", false);
 		System.out.println("\t\tWelcome to Client\n\n");
-		if (!Functions.initClient()) {
-			System.out.println("\n\n\t Looks like your config file is "
-					+ "not formated as we expect");
+		if(!Functions.initClient()) {
+			System.out.println("\n\n\t looks like your config file is " + 
+								"not formated as we expect");
 			return;
 		}
-		socket = new DatagramSocket(clientPort);
 		MOD = 3 * window;
+		socket = new DatagramSocket(clientPort);
 		packetsReceived = new DatagramPacket[MOD];
-		while (true) {
+		while(true)
+		{
 			byte[] BUFFER = new byte[600];
-			DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
+			DatagramPacket packet = new DatagramPacket(BUFFER,BUFFER.length);
 			socket.receive(packet);
-			if (packet.getLength() == 0)
-				break;
-			byte num = getSeqnum(packet);
 			CRC32 checksum = new CRC32();
 			checksum.update(packet.getData());
+			byte num = getSeqnum(packet);
 			System.out.println("received " + num + " checksum in decimal = "
 				+ checksum.getValue() + " checksum in binary = " 
 									  + toBinary(checksum.getValue()));
-			if (num == awaiting) {
+			if(packet.getLength() == 0)
+				break ;
+			if(num == awaiting)
+			{
 				packetsReceived[awaiting] = new DatagramPacket(
 						packet.getData(), packet.getLength(),
 						packet.getAddress(), packet.getPort());
-				for (int i = 0; i < window && packetsReceived[awaiting] != null; ++i) {
+				for (int i = 0; i < window && packetsReceived[awaiting] != null
+						; ++i) 
+				{
 					byte[] wr = packetsReceived[awaiting].getData();
 					packetsReceived[awaiting] = null;
-					System.out.println("write in file packet with sequ num = "
-										+ wr[0]);
+					System.out.println("write in file packet with sequ num = " 
+												+ wr[0]);
 					WriteFile(wr);
 					IncWait();
 				}
-			} else if(num >= awaiting && num < (awaiting + window) % MOD) {
-				packetsReceived[num] = new DatagramPacket(packet.getData(),
-						packet.getLength(), packet.getAddress(),
-						packet.getPort());
+				// to be cumulative the loop will get me the last in sequence 
+				// as it write it to file
 			}
-			ACK((byte)((awaiting-1 + MOD)%MOD));
+			else if( num >= awaiting && num < (awaiting+window)%MOD)
+			{
+				packetsReceived[num] = new DatagramPacket(
+						packet.getData(), packet.getLength(),
+						packet.getAddress(), packet.getPort());
+			}
+				
+			ACK((byte)((awaiting+MOD-1)%MOD));
 		}
 		socket.close();
 
--- src/GoBackN/Server.java
+++ src/GoBackN/Server.java
@@ -8,42 +8,44 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Server {
-	public static int serverPort = 7672;
 	public static int clientPort;
+	public static int serverPort;
 	public static int window;
 	public static DatagramSocket socket;
 	public static boolean acked[];
+	private static Thread mytimer = null;
 	public static int pointerNext;
 	static ArrayList<DatagramPacket> packets;
 	static byte[] data;
+	private static byte waitACK = 0;
 	private static int fileIndex = 0;
-	private static int last_rec;
-
-	public static double probability;
-
+	private static int currentFileIndex = 0 ;
+	
 	public static InetAddress ip;
-
-	public static byte MOD;
-
+	
+	public static double probability;
+	
+	public static byte MOD ;
+	
 	public static byte[] ReadFile() throws IOException {
 		byte[] data;
 		Path path = Paths.get("output.txt");
 		data = Files.readAllBytes(path);
 		return data;
 	}
-
+	
 	public static synchronized byte getSeqnum(DatagramPacket packet) {
 		return packet.getData()[0];
 	}
-
+	
 	public static void CreatePackets() throws IOException {
 		packets = new ArrayList<DatagramPacket>();
 		byte[] data = ReadFile();
 		byte sequ = 0;
-		for (int i = 0; i < data.length;) {
+		for(int i = 0; i < data.length;) {
 			byte[] buffer = new byte[512];
 			buffer[0] = sequ;
 			sequ++;
@@ -55,58 +57,66 @@ public class Server {
 					clientPort));
 		}
 	}
-
+	
 	public synchronized static void SendPacket(int physicalNumber)
 			throws IOException {
 		double num = Math.random();
-		if (num > probability) {
+		if(num > probability) {
 			socket.send(packets.get(physicalNumber));
 			System.out.println("send packet "
 					+ packets.get(physicalNumber).getData()[0]);
-		} else System.out.println("drop packet " + 
-					packets.get(physicalNumber).getData()[0]);
+		} else
+			System.out.println("Drop packet " + packets.get(physicalNumber).getData()[0]);
 	}
 	
-	private static int in_range(int rec) {
-		for(int i = last_rec, j = 0 ; j < window ; ++j) {
-			if(i == rec) return j+1;
-			++i;
-			i %= MOD;
-		}
-		return -1;
+	private static void IncACK() {
+		++waitACK;
+		waitACK %= MOD;
 	}
-
-	public static void main(String[] args) throws IOException, InterruptedException {
-		System.out.println("\t\tWelcome to Server\n\n");
-		if (!Functions.initServer()) {
-			System.out.println("\n\n\t looks like your config file is "
-					+ "not formated as we expect");
+	
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("Server Window size");
+		if(!Functions.initServer()) {
+			System.out.println("\n\n\t looks like your config file is " + 
+								"not formated as we expect");
 			return;
 		}
-		MOD = (byte) (3 * window);
+		MOD = (byte) (3*window) ;
 		CreatePackets();
 		socket = new DatagramSocket(serverPort);
-		for (fileIndex = 0; fileIndex < window && fileIndex < packets.size(); ++fileIndex) {
+		acked = new boolean[MOD];
+		
+		for(fileIndex = 0; fileIndex < window && fileIndex < packets.size();
+				++fileIndex) {
 			SendPacket(fileIndex);
 		}
-		fileIndex = 0;
-		last_rec = 0;
-		int t = 0, o = -1;
-		acked = new boolean[MOD];
+		mytimer = new Thread(new Timer(waitACK,currentFileIndex));
+		mytimer.start();
 		byte[] BUFFER = new byte[15];
-		DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
-		byte num;
-		while (fileIndex < packets.size()) {
+		DatagramPacket packet = new DatagramPacket(BUFFER,BUFFER.length);
+		byte num ;
+		while(currentFileIndex < packets.size())
+		{
+			System.out.println("waiting ACK of " + waitACK);
 			socket.receive(packet);
 			num = getSeqnum(packet);
-			System.out.println("Received ACK for " + num);
-			if(num < o) ++t;// add 1 to cycles
-			o = num; // new offset
-			for(int i = t*MOD + o + 1, j = 0 ; i < packets.size() && 
-								j < window ; ++j, ++i)
-				SendPacket(i);
+			System.out.println("ACK for "+ num+" awaiting " + waitACK);
+			mytimer.interrupt();
+			while(waitACK != num)
+			{
+				IncACK();
+				currentFileIndex++;
+				if(fileIndex < packets.size())
+					SendPacket(fileIndex++);
+			}
+			mytimer = new Thread(new Timer(waitACK,currentFileIndex));
+			mytimer.start();
 		}
-		socket.close();
+		DatagramPacket p = new DatagramPacket(null,0,ip,clientPort);
+		socket.send(p);
+		
+		socket.close();	
 	}
 
 }
